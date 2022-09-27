a4f49c4e6174fcf330f438b8ebcdaa450bd9671d
mac <mac@MacBook-Pro.local>
2022-09-26 22:20:29 -0500

    Added some changes for columns and rows

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,31 +19,55 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         // This if to define the layout. Instead of an xml file you call composable functions
         setContent {
-            //Note mine: Remember that this name "ComposeTutorialTheme" is based on the name
-            //given to the project. It styles the composable functions
+            // Note mine: Remember that this name "ComposeTutorialTheme" is based on the name
+            // given to the project. It styles the composable functions
             ComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
 }
 
-//Preview is used to use Android Studio Preview
-@Preview(showBackground = true)
+// Material components such as Surface automatically are built to make
+// the experience better, so for instance it can change a text color if the background change of
+// color to make it more visible
+
+@Composable
+fun Greeting(name: String) {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            //Modifiers tell a UI element how to lay out, display or behave within is parent layout
+            // there are dozen of modifiers
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
+        }
+
+    }
+}
+
+//@Preview is used to use Android Studio Preview
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposeTutorialTheme {
-        Greeting("Android")
+        MyApp()
     }
 }

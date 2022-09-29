8ba91bbda0a443122151643b725b6ac7c338fc8b
redleafar <raf_berm2@yahoo.com>
2022-09-28 22:09:09 -0500

    State in compose

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,6 +45,15 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
+    // "Remember" is used to guard against recomposition so the state is not reset.
+    // Internal states like this work as "private" variables in a class, so
+    // you can have different UI elements each with its own version of the state
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    // The composable function will automatically be "subscribed" to the state. If the state
+    // changes, composables that read these fields will be recomposed to display the updates
+
     // A surface container using the 'background' color from the theme
     Surface(
         color = MaterialTheme.colors.primary,
@@ -51,12 +62,19 @@ fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             //Modifiers tell a UI element how to lay out, display or behave within is parent layout
             // there are dozen of modifiers
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(
+                    if (expanded.value)
+                        "Show less"
+                    else "Show more"
+                )
             }
         }
 
@@ -71,3 +89,9 @@ fun DefaultPreview() {
         MyApp()
     }
 }
+
+// Compose transform data into UI by calling composable functions. The data changes re-execute
+// these functions with new data. That is recomposition. Compose only recompose components
+// whose data has changed
+// Composable functions can execute frequently and in any order, you must not rely on the
+// ordering in which the code is executed, or on how many times this function will be recomposed

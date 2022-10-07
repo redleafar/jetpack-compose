cc4dfc78c18e245466c24f550630a8cb7374a5b1
redleafar <raf_berm2@yahoo.com>
2022-10-06 20:35:04 -0500

    Moved strings

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -131,9 +132,7 @@ fun Greeting(name: String) {
             }
             OutlinedButton(onClick = { expanded = !expanded }) {
                 Text(
-                    if (expanded)
-                        "Show less"
-                    else "Show more"
+                    if (expanded) stringResource(id = R.string.show_less) else stringResource(id = R.string.show_more)
                 )
             }
         }
